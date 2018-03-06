98133502b743451c7656230501a1683ec18a0be7
Mohammad Maswadeh <mt.maswadeh@gmail.com>
2018-03-06 18:52:43 +0200

    Initial Commit

---
+++ CloudConnectiv-Task/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudConnectiv-Task
+//
+//  Created by Mohammad Maswadeh on 3/6/18.
+//  Copyright © 2018 AFK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudConnectiv-Task/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudConnectiv-Task
+//
+//  Created by Mohammad Maswadeh on 3/6/18.
+//  Copyright © 2018 AFK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
